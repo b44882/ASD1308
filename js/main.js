ae7282bb7ceaa1d620f156feb41dd0c666a82b37
Brett Gear <bgear@fullsail.edu>
2013-08-12 15:47:45 -0400

    json test push

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 $(function(){
-	
 	$("#get_json").bind("click", function(){
 		$.ajax({
 			url: "js/data.json",
 			type: "GET",
 			dataType: "json",
 			success: function(data, status){
-				console.log(status,data);
+				console.log(status, data);
 			},
-			error: function(error, parseerror){
-				console.log("Error:" + error + "\n" + "Parse Error: " + parseerror);
+			error: function(error, perror){
+				console.log("Error:" + error + "\n" + "Parse Error: " + perror);
 			}
 		});
 	
