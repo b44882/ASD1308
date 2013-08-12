e0ad05effc21da5ed64c65bc109c7e88c25d01bc
Brett Gear <bgear@fullsail.edu>
2013-08-11 23:09:46 -0400

    adding simple json error because of access-control-allow-orgin error.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-//javascript
+$(function(){
+	
+	$("#get_json").bind("click", function(){
+		$.ajax({
+			url: "js/data.json",
+			type: "GET",
+			dataType: "json",
+			success: function(data, status){
+				console.log(status,data);
+			},
+			error: function(error, parseerror){
+				console.log("Error:" + error + "\n" + "Parse Error: " + parseerror);
+			}
+		});
+	
+	});
+
+});
+        
+        	
+    
